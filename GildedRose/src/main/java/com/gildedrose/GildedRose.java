21abb08a7d250af7a33cc650822e7859ebe2a6b8
ZeDarkNights <48436731+ZeDarkNights@users.noreply.github.com>
2020-09-15 17:14:36 +0200

    Update GildedRose.java

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -38,7 +38,7 @@ class GildedRose {
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
-
+			//refaire comme au début, utiliser liste
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -51,6 +51,7 @@ class GildedRose {
                         items[i].quality = 0;
                     }
                 } else {
+					//Brie +2 quality quand périmé
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
                     }
